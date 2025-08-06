4d4d5475fbda708f5051e591367bf7f1495953f5
MichaelCodes192 <mkang4636@gmail.com>
2025-08-06 14:01:00 -0700

    Create Message.js

---
+++ models/Message.js
@@ -0,0 +1,16 @@
+// models/Message.js
+const mongoose = require('mongoose');
+
+const messageSchema = new mongoose.Schema({
+  sender: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+  },
+  text: String,
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model('Message', messageSchema);
