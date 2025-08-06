d410f69588edd764ff2a856d057da149c82b8fbd
MichaelCodes192 <mkang4636@gmail.com>
2025-08-06 13:57:10 -0700

    Create contact.js

---
+++ routes/contact.js
@@ -0,0 +1,9 @@
+// routes/contact.js
+const express = require('express');
+const router = express.Router();
+const contactController = require('../controllers/contactController');
+
+router.get('/', contactController.getContactForm);
+router.post('/', contactController.submitContactForm);
+
+module.exports = router;
