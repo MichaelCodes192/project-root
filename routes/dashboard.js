01004015018791d74ff292c3d26d4f4f25d7647f
MichaelCodes192 <mkang4636@gmail.com>
2025-08-06 13:25:46 -0700

    Create dashboard.js

---
+++ routes/dashboard.js
@@ -0,0 +1,9 @@
+// routes/dashboard.js
+const express = require('express');
+const router = express.Router();
+const { isLoggedIn } = require('../middleware/authMiddleware');
+const dashboardController = require('../controllers/dashboardController');
+
+router.get('/', isLoggedIn, dashboardController.getDashboard);
+
+module.exports = router;
