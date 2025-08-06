09adf854294927067a3fc41b49573b6466911f79
MichaelCodes192 <mkang4636@gmail.com>
2025-08-06 13:47:30 -0700

    Create adminMiddleware.js

---
+++ middleware/adminMiddleware.js
@@ -0,0 +1,8 @@
+// middleware/adminMiddleware.js
+exports.isAdmin = (req, res, next) => {
+  if (req.session.user && req.session.user.isAdmin) {
+    next();
+  } else {
+    res.status(403).send('Forbidden');
+  }
+};
